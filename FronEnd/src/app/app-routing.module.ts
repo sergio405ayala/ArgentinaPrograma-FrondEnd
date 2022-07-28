17449f046f9d1162454e24a74c64c26dca3d0d3c
sergio405ayala <99102058+sergio405ayala@users.noreply.github.com>
2022-07-27 23:46:23 -0300

    actualizacion

--- FronEnd/src/app/app-routing.module.ts
+++ FronEnd/src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EditExperienciaComponent } from './components/experiencia/edit-experiencia.component';
 import { NewExperienciaComponent } from './components/experiencia/new-experiencia.component';
+import { EditEducacionComponent } from './components/educacion/edit-educacion.component';
+import { NewEducacionComponent } from './components/educacion/new-educacion.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 
@@ -9,7 +11,9 @@ const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'nuevaexp', component: NewExperienciaComponent},
-  { path: 'editexp/:id', component: EditExperienciaComponent}
+  { path: 'editexp/:id', component: EditExperienciaComponent},
+  { path: 'nuevaeducacion', component: NewEducacionComponent},
+  { path: 'editeducacion/:id', component: EditEducacionComponent}
 ];
 
 @NgModule({
@@ -17,3 +21,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
--- FronEnd/src/app/app.module.ts
+++ FronEnd/src/app/app.module.ts
@@ -20,6 +20,8 @@ import { LoginComponent } from './components/login/login.component';
 import { interceptorProvider } from './service/interceptor-service';
 import { NewExperienciaComponent } from './components/experiencia/new-experiencia.component';
 import { EditExperienciaComponent } from './components/experiencia/edit-experiencia.component';
+import { NewEducacionComponent } from './components/educacion/new-educacion.component';
+import { EditEducacionComponent } from './components/educacion/edit-educacion.component';
 
 @NgModule({
   declarations: [
@@ -37,6 +39,8 @@ import { EditExperienciaComponent } from './components/experiencia/edit-experien
     LoginComponent,
     NewExperienciaComponent,
     EditExperienciaComponent,
+    NewEducacionComponent,
+    EditEducacionComponent,
   ],
   imports: [
     BrowserModule,
